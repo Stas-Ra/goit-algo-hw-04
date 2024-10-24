7e97c663c2e0a1702837f7d081d54325ac35914a
Stanislav Rakytianskyi <mail.sovik@gmail.com>
2024-10-24 09:46:59 +0300

    Add files via upload

---
+++ chw_4_task_2.py
@@ -0,0 +1,35 @@
+# with open("G:\PROJECTS\.git\PROJECTS\PROJECTS\PROJECTS\cats_info.txt", "w") as file:
+#     file.write(
+#         """60b90c1c13067a15887e1ae1,Tayson,3
+# 60b90c2413067a15887e1ae2,Vika,1
+# 60b90c2e13067a15887e1ae3,Barsik,2
+# 60b90c3b13067a15887e1ae4,Simon,12
+# 60b90c4613067a15887e1ae5,Tessi,5
+# """
+#     )
+
+
+from pathlib import Path
+
+def get_cats_info(path):
+    with open(path, "r", encoding="utf-8", errors="replace") as file:
+        if not file:
+            return "No file available"
+        else:
+            cats_info = []
+            for line in file:
+                cat_info = {"id": "", "name": "", "age": ""}
+                # line = file.read()
+                # cat_info = {"id": "", "name": "", "age": ""}
+                cat_info["id"] = line.split(",")[0]
+                cat_info["name"] = line.split(",")[1]
+                cat_info["age"] = line.split(",")[2].strip()
+                # return cat_info
+                cats_info.append(cat_info)
+            return cats_info
+
+print(get_cats_info("G:\PROJECTS\.git\PROJECTS\PROJECTS\PROJECTS\cats_info.txt"))
+            
+
+
+                
